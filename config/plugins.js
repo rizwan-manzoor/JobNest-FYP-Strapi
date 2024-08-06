923ace7951d3364666828924215917bb3014363d
rizwan-m-manzoor <m.rizwan@datics.ai>
2024-08-06 11:21:55 +0500

    Basic Users Authentication and role based authorization configured with Email confirmation using sendGrid email service.

---
+++ config/plugins.js
@@ -0,0 +1,14 @@
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: "sendgrid", // For community providers pass the full package name (e.g. provider: 'strapi-provider-email-mandrill')
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+      settings: {
+        defaultFrom: env("SENDGRID_EMAIL_FROM"),
+        defaultReplyTo: env("SENDGRID_EMAIL_TO"),
+      },
+    },
+  },
+});
